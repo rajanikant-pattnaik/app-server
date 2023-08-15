00acf9ee3d479519f076417566e1f64267bff601
unknown <rjpattnaik12345@gmail.com>
2023-08-15 14:30:27 +0530

    another commit

--- controllers/bios.js
+++ controllers/bios.js
@@ -1,30 +1,53 @@
-import {bio} from '../models/bios.js'
+import { bio } from "../models/bios.js";
 
-export const addAndEdit=async(req,res)=>{
-    const {about,skills,prof_details,certifications,experience,education}=req.body;
-    const userId=req.currUser._id;
-    const data=await bio.findOne({userId});
-    if(!data){
-      const updatedBio=await bio.create({
-        userId,about,skills,prof_details,certifications,experience,education
-      })
-      return res.status(200).json({
-        sucess:true,
-        message:"data is added",
-        updatedBio
-      })
-    }
-    data.userId=userId;
-    data.about=about;
-    data.skills=skills;
-    data.prof_details=prof_details;
-    data.certifications=certifications;
-    data.experience=experience;
-    data.education=education;
-    await data.save();
+export const addAndEdit = async (req, res) => {
+  const { about, skills, prof_details, certifications, experience, education } =
+    req.body;
+  const userId = req.currUser._id;
+  const data = await bio.findOne({ userId });
+  if (!data) {
+    const updatedBio = await bio.create({
+      userId,
+      about,
+      skills,
+      prof_details,
+      certifications,
+      experience,
+      education,
+    });
     return res.status(200).json({
-        sucess:true,
-        message:"data is updated",
-        data
-      })
+      sucess: true,
+      message: "data is added",
+      updatedBio,
+    });
+  }
+  data.userId = userId;
+  data.about = about;
+  data.skills = skills;
+  data.prof_details = prof_details;
+  data.certifications = certifications;
+  data.experience = experience;
+  data.education = education;
+  await data.save();
+  return res.status(200).json({
+    sucess: true,
+    message: "data is updated",
+    data,
+  });
+};
+
+export const getBio=async(req,res)=>{
+  const {userId}=req.body;
+  const data=await bio.findOne({userId});
+  if(!data){
+    return res.status(500).json({
+      success:false,
+      message:"Bio is not present",
+    })
+  }
+  res.status(200).json({
+    success:true,
+    message:"Bio is retrieved",
+    data
+  })
 }

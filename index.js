a8890cca3f244ef574eecd182360914309873e60
unknown <rjpattnaik12345@gmail.com>
2023-08-16 16:43:04 +0530

    another

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin:[process.env.NODE_ENV],
+  origin:[process.env.FRONTEND_URL],
   methods:["GET","POST","PUT","DELETE"],
   credentials:true
 }));

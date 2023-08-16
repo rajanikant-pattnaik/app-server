d1a2fb841e177db9a8ccdba034a0a0e472050f1c
unknown <rjpattnaik12345@gmail.com>
2023-08-16 12:31:22 +0530

    another commit

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ connectDb();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use("*",cors({
+  origin:true,
+  credentials:true
+}));
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/users/bios", bioRouter);
 app.use("/api/v1/users/connect", connectRouter);
--- utils/features.js
+++ utils/features.js
@@ -5,9 +5,7 @@ export const sendCookie = (newUser, res, message, statusCode = 200) => {
 
   res.status(statusCode).cookie('token', token, {
     httpOnly: true,
-    maxAge: 15 * 60 * 1000,
-    sameSite: process.env.NODE_ENV === 'Development' ? 'Lax' : 'none',
-    secure: process.env.NODE_ENV === 'Development' ? false : true,
+    maxAge: 15 * 60 * 1000
   }).json({
     success: true,
     message,

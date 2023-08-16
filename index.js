b0ce08913e4d6755dcdcdf1b129c413b893c5900
unknown <rjpattnaik12345@gmail.com>
2023-08-16 20:25:43 +0530

    another commit

--- index.js
+++ index.js
@@ -12,11 +12,7 @@ connectDb();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin:[process.env.FRONTEND_URL],
-  methods:["GET","POST","PUT","DELETE"],
-  credentials:true
-}));
+app.use(cors());
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/users/bios", bioRouter);
 app.use("/api/v1/users/connect", connectRouter);
